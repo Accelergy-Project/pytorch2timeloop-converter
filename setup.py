cbe42edb1c31475f871cb260050bb0bca74fde44
Jaeyeon Won <jaeyeon@mit.edu>
2023-02-01 21:42:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='pytorch2timeloop',
         version='0.2',
         url='https://github.com/Accelergy-Project/pytorch2timeloop-converter',
         license='MIT',
-        install_requires=["torch>=1.7", "torchvision>=0.8", "numpy>=1.21", "pyyaml>=5.3", "transformers>=4.26.0"],
+        install_requires=["torch==1.8", "torchvision==0.8", "numpy==1.21", "pyyaml==5.3", "transformers==4.26.0"],
         python_requires='>=3.6',
         include_package_data=True,
         packages=find_packages())
