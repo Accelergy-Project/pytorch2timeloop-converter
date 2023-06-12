b6a248039d30b8cc7e8f3af0eef5a408fcf40a28
Michael Gilbert <gilbertm@mit.edu>
2023-06-12 18:36:09 -0400

    Add matmul and binary elementwise func

--- pytorch2timeloop/utils/interpreter.py
+++ pytorch2timeloop/utils/interpreter.py
@@ -1,4 +1,5 @@
 import logging
+import math
 import operator
 from typing import Dict, Tuple, Union
 
@@ -6,7 +7,8 @@ import torch
 from torch import nn
 import torch.fx as fx
 
-from .converter import generate_description, generate_func_description
+from .converter import generate_description, generate_matmul_func
+from pytorch2timeloop.utils.layer_descriptions import BinaryElementwiseFuncDescription
 
 logger = logging.getLogger(__name__)
 
@@ -21,8 +23,24 @@ class Converter(fx.Interpreter):
         nn.ReLU,
         nn.ReLU6
     ]
+
     DEFAULT_IGNORED_MODULES = Union[
         nn.AdaptiveAvgPool2d
+    ]
+
+    UNARY_ELEMENTWISE_FUNC = [
+        math.sqrt
+    ]
+
+    BINARY_ELEMENTWISE_FUNC = [
+        operator.add,
+        torch.add,
+        operator.sub,
+        torch.sub,
+        operator.mul,
+        torch.mul,
+        operator.truediv,
+        torch.div
     ]
 
     def __init__(self, module, garbage_collect_values=True,
@@ -81,14 +99,35 @@ class Converter(fx.Interpreter):
                       original_args: tuple):
         result = super().call_function(target, args, kwargs)
 
-        if target == operator.add or target == torch.add:
-            description = generate_func_description(
-                target,
-                args[0],
-                result,
-                name,
-                original_args[0].name
+        if target in Converter.BINARY_ELEMENTWISE_FUNC:
+            if isinstance(args[1], torch.Tensor):
+                description = BinaryElementwiseFuncDescription(
+                    ifmap1_shape = args[0].shape,
+                    ifmap2_shape = args[1].shape,
+                    ofmap_shape = result.shape,
+                    ifmap1_name = f'{original_args[0].name}.out',
+                    ifmap2_name = f'{original_args[1].name}.out',
+                    ofmap_name = f'{name}.out',
+                    name = name
+                )
+                self.summary.append(description)
+            logger.warning('assuming op by scalar %s[type=%s] args=%s',
+                           name, target, args)
+        elif target == torch.matmul:
+            description = generate_matmul_func(
+                input1=args[0],
+                input2=args[1],
+                output=result,
+                name=name,
+                input1_name=f'{original_args[0].name}.out',
+                input2_name=f'{original_args[1].name}.out'
             )
-            self.summary.append(description)
+        elif target == nn.softmax:
+            raise NotImplementedError('softmax unimplemented')
+        elif target in Converter.UNARY_ELEMENTWISE_FUNC:
+            pass
+        else:
+            logger.error('unknwown function  %s[type=%s]', name, target)
+            raise NotImplementedError()
 
         return result
