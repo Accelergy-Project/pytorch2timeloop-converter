1af245c54ef7b4f7b82a0d6b15eacc6eeaee7f6b
Michael Gilbert <gilbertm@mit.edu>
2023-06-12 17:35:21 -0400

    Add common CNNs as tests

---
+++ test/test_mobilenet_v3.py
@@ -0,0 +1,23 @@
+import unittest
+
+from torchvision.models import mobilenet_v3_small
+import pytorch2timeloop
+
+from .test_configs import TMP_TEST_DIR
+
+class TestMobileNetv3(unittest.TestCase):
+    def setUp(self):
+        self.net = mobilenet_v3_small()
+        self.input_size = (3, 224, 224)
+        self.batch_size = 1
+
+    def test_mobilenet_v3_small(self):
+        pytorch2timeloop.convert_model(
+            model=self.net,
+            input_size=self.input_size,
+            batch_size=self.batch_size,
+            convert_fc=False,
+            model_name='mobilenet_v3_small',
+            save_dir=TMP_TEST_DIR,
+            exception_module_names=[]
+        )
